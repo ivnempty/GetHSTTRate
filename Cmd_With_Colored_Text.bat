fc7a9912917035036ed4cfcdc2261b9d8effcc77
Ivien <ivnempty@gmail.com>
2018-08-27 12:27:04 +0800

    Batch Script to Show Colored Text in CMD

---
+++ Cmd_With_Colored_Text.bat
@@ -0,0 +1,18 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set "DEL=%%a"
+)
+
+call :ColorText 0a "Please input commit message"
+echo. 
+set /p var1=
+echo %var1%
+
+goto :eof
+:ColorText
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1
+goto :eof
